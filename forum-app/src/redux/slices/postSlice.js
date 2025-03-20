d8edf1f275fd27b10139a88fa2dfb5d82bfa8606
michaelyuen89 <michaelyuen89@users.noreply.github.com>
2025-03-20 00:58:36 -1000

    add headers to fetchPost function

--- forum-app/src/redux/slices/postSlice.js
+++ forum-app/src/redux/slices/postSlice.js
@@ -1,13 +1,19 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5002/posts";
+const API_URL = "http://127.0.0.1:5009/posts";
 
 // Async thunk to fetch posts
-export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
-  const response = await axios.get(API_URL);
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async (_, { getState }) => {
+  const token = getState().auth.token; // Assuming the token is stored in the Redux auth slice
+  const response = await axios.get(API_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data.filter((post) => post.post.status === "PostStatus.PUBLISHED");
 });
+
 
 const postsSlice = createSlice({
   name: "posts",
